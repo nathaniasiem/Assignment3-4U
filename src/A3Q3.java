453d54a151c5ef60df7d051088d5f67777bf940c
candy <candy@192.168.0.21>
2017-10-07 20:38:32 -0400

    07/10/2017-A3Q3 initial commit

---
+++ src/A3Q3.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author candy
+ */
+public class A3Q3 {
+public int countOnes(int[]array){
+    
+}
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
