73bbb45ed62cad8b21ee570bfc6bbcfa24606f69
simon7323 <simon7323@HRH-IC0027344.wrdsb.ca>
2017-10-10 10:03:01 -0400

    10/10/2017-A3Q5 added comments

--- src/A3Q5.java
+++ src/A3Q5.java
@@ -10,12 +10,17 @@
  */
 public class A3Q5 {
     public void insertionSort(String [] word){
+        //go through the array of word
         for (int i=0;i<word.length;i++){
+            //create a variable to keep track the position
             int spot = i;
+            //keep program running when position
             while(spot>0 && word[spot-1].compareToIgnoreCase(word[spot])>0){
+                //swap integers according  to the alphabetical order
                 String temp = word[spot];
                 word[spot]=word[spot-1];
                 word[spot-1]=temp;
+                //decreases until reach the very last word
                 spot--;
             }
         }
