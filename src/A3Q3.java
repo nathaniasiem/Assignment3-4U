440e6b9858be39d79d1927ffee0393502b3742fc
candy <candy@192.168.0.20>
2017-10-10 16:56:02 -0400

    10/10/2017-A3Q3 completed with comments

--- src/A3Q3.java
+++ src/A3Q3.java
@@ -12,18 +12,23 @@ public class A3Q3 {
     public int countOnes(int[]array){
         //create a base case to input the numbers
         int sum=0;
+        //go through the set of numbers
         for (int i=0;i<array.length;i++){
+            //condition when the integer in the array equals one
             if(array[i]==1){
+                //add towards the sum
                 sum++; 
         }
-        
+        //other than the value of 1
         }return sum;
     
 }
     public static void main(String[] args) {
         // create variable to test boundaries
         A3Q3 test= new A3Q3();
+        //create an array of numbers
          int[] array = {0,0,0,1,1,1};
+         //call and print out the method
         System.out.println(test.countOnes(array));  
     }
     
