b7e81f49e2bc53baaed595279c217bdf08e8894e
simon7323 <simon7323@HRH-IC0027344.wrdsb.ca>
2017-10-10 10:10:40 -0400

    10/10/2017-A3Q3 completed but needs comments

--- src/A3Q3.java
+++ src/A3Q3.java
@@ -9,11 +9,22 @@
  * @author candy
  */
 public class A3Q3 {
-public int countOnes(int[]array){
+    public int countOnes(int[]array){
+        //create a base case to input the numbers
+        int sum=0;
+        for (int i=0;i<array.length;i++){
+            if(array[i]==1){
+                sum++; 
+        }
+        
+        }return sum;
     
 }
     public static void main(String[] args) {
-        // TODO code application logic here
+        // create variable to test boundaries
+        A3Q3 test= new A3Q3();
+         int[] array = {0,0,0,1,1,1};
+        System.out.println(test.countOnes(array));  
     }
     
 }
